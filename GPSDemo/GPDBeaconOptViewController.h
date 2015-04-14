32ffaf069357a539483668a76216ec7a6b99f571
Donna Tom <donnatom@gmail.com>
2015-04-13 20:31:15 -0500

    Add initial classes/xib for beacon opt in VC.
    * GPDBeaconOptViewController.xib just has a UISwitch in it at the moment.
    * GPDBeaconOptViewController.h/.m classes contain default boilerplate with an IBOutlet for the switch.

---
+++ GPSDemo/GPDBeaconOptViewController.h
@@ -0,0 +1,15 @@
+//
+//  GPDBeaconOptViewController.h
+//  GPShopper-iOS
+//
+//  Created by Donna on 4/12/15.
+//  Copyright (c) 2015 James Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPDBeaconOptViewController : UIViewController
+
+@property(weak, nonatomic) IBOutlet UISwitch* optInSwitch;
+
+@end
