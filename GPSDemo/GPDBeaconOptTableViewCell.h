4090e6033e41a21ece18900e9d058495d911ffbf
Donna Tom <donnatom@gmail.com>
2015-04-14 00:34:17 -0500

    Add new class for the beacon table cell.
    * Overrides initWithStyle:reuseIdentifier to register for beacon opt in/out notifications

---
+++ GPSDemo/GPDBeaconOptTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GPDBeaconOptTableViewCell.h
+//  GPShopper-iOS
+//
+//  Created by Donna on 4/13/15.
+//  Copyright (c) 2015 James Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPDBeaconOptTableViewCell : UITableViewCell
+
+@end
